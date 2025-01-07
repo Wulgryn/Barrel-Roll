ce63f3bff979e232d83f961754a1dd9daefb9c39
Wulgryn <urbanzolika2004@gmail.com>
2025-01-07 20:41:15 +0100

    Add project files.

---
+++ Barrel Roll CLI Test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Barrel_Roll_CLI_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
